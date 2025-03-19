bd784e42f3f51e22a873e4718fa3cb7d0de9a95e
mayankmehta2004 <yadavmayank1980@gmail.com>
2025-03-20 00:51:01 +0530

    Create oueue using stack.java

---
+++ oueue using stack.java
@@ -0,0 +1,31 @@
+class MyQueue {
+    Stack <Integer> first;
+    Stack <Integer> second;
+
+    public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+    
+    public void push(int x) {
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        first.push(x);
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+    }
+    
+    public int pop() {
+        return first.pop();
+    }
+    
+    public int peek() {
+        return first.peek();
+    }
+    
+    public boolean empty() {
+        return first.isEmpty();
+    }
+}
